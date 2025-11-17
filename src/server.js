092df4461a5e8e47840845016eff1725e639bed1
Burak Ezer <burakezer@outlook.com>
2025-11-17 16:04:24 +0300

    2

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsroute from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export function setupServer() {
   const app = express();
 
-  dotenv.config();
+  //dotenv.config();
   const PORT = Number(env('PORT', 5000));
   app.use(express.json());
 
@@ -22,38 +24,11 @@ export function setupServer() {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const data = await getAllContacts();
-    res.json(data);
-  });
+  app.use(contactsroute);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+  app.use(errorHandler);
 
-    if (!contact) {
-      res.status(404).json({
-        message: 'No contact',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      data: contact,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Route not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-    });
-  });
+  app.use(notFoundHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
