1776b7d1135908c5612c422f5f2a72526524413d
Burak Ezer <burakezer@outlook.com>
2025-12-11 19:26:36 +0300

    6fix3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,8 +47,21 @@ export const contactController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
-  const contact = await createContact(req.body);
+  const photo = req.file;
 
+  let photoUrl;
+
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+  const contact = await createContact({
+    ...req.body,
+    photo: photoUrl,
+  });
   res.status(201).json({
     status: 201,
     message: `Successfully created a contact!`,
